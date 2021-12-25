ce4c1e28e6fab8c86f71196b4ab0fe8187b7a3a9
ssdev-org <96655975+ssdev-org@users.noreply.github.com>
2021-12-25 17:09:58 +0100

    Add files via upload

---
+++ tiles_png/SSDeV/SSDeVWimpel.tsx
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="SSDeVWimpel" tilewidth="640" tileheight="640" tilecount="14" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="WimpelSSDeV.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="WimpelSSDeV2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="WimpelSSDeV4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="SSD_LOGO5_2014tief64.png"/>
+ </tile>
+ <tile id="7">
+  <image width="640" height="640" source="SSD_LOGO5_2014relief640.png"/>
+ </tile>
+ <tile id="8">
+  <image width="640" height="640" source="SSD_LOGO5_2014relief640trans.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="WimpelSSDeVrot.png"/>
+  <animation>
+   <frame tileid="0" duration="450"/>
+   <frame tileid="13" duration="450"/>
+   <frame tileid="14" duration="450"/>
+   <frame tileid="15" duration="450"/>
+   <frame tileid="12" duration="450"/>
+  </animation>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="WimpelSSDeVgruen.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="WimpelSSDeVblau.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="WimpelSSDeVviolett.png"/>
+ </tile>
+ <tile id="20">
+  <image width="64" height="96" source="SSD_LOGO5_2014schraeg64.png"/>
+ </tile>
+ <tile id="21">
+  <image width="64" height="96" source="SSD_LOGO5_2014schraeg64r.png"/>
+ </tile>
+ <tile id="23">
+  <image width="64" height="96" source="WimpelSSDeV2Fuss.png"/>
+ </tile>
+ <tile id="24">
+  <image width="64" height="96" source="WimpelSSDeV2Schaedel.png"/>
+ </tile>
+</tileset>
---
+++ tiles_png/SSDeV/SSDeVWimpel2.tsx
@@ -0,0 +1,100 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="SSDeVWimpel" tilewidth="640" tileheight="640" tilecount="23" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="WimpelSSDeV.png"/>
+  <animation>
+   <frame tileid="33" duration="450"/>
+   <frame tileid="32" duration="450"/>
+   <frame tileid="31" duration="450"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="WimpelSSDeV2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="WimpelSSDeV4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="SSD_LOGO5_2014tief64.png"/>
+ </tile>
+ <tile id="7">
+  <image width="640" height="640" source="SSD_LOGO5_2014relief640.png"/>
+ </tile>
+ <tile id="8">
+  <image width="640" height="640" source="SSD_LOGO5_2014relief640trans.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="WimpelSSDeVrot.png"/>
+  <animation>
+   <frame tileid="0" duration="450"/>
+   <frame tileid="13" duration="450"/>
+   <frame tileid="14" duration="450"/>
+   <frame tileid="15" duration="450"/>
+   <frame tileid="12" duration="450"/>
+  </animation>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="WimpelSSDeVgruen.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="WimpelSSDeVblau.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="WimpelSSDeVviolett.png"/>
+ </tile>
+ <tile id="20">
+  <image width="64" height="96" source="SSD_LOGO5_2014schraeg64.png"/>
+ </tile>
+ <tile id="21">
+  <image width="64" height="96" source="SSD_LOGO5_2014schraeg64r.png"/>
+ </tile>
+ <tile id="23">
+  <image width="64" height="96" source="WimpelSSDeV2Fuss.png"/>
+ </tile>
+ <tile id="24">
+  <image width="64" height="96" source="WimpelSSDeV2Schaedel.png"/>
+ </tile>
+ <tile id="25">
+  <image width="32" height="32" source="SSD_LOGO5_201432magenta.png"/>
+  <animation>
+   <frame tileid="25" duration="700"/>
+   <frame tileid="26" duration="700"/>
+   <frame tileid="27" duration="700"/>
+  </animation>
+ </tile>
+ <tile id="26">
+  <image width="32" height="32" source="SSD_LOGO5_201432blau.png"/>
+ </tile>
+ <tile id="27">
+  <image width="32" height="32" source="SSD_LOGO5_201432tuekis.png"/>
+ </tile>
+ <tile id="28">
+  <image width="64" height="96" source="SSD_LOGO5_2014schraeg64rmagenta.png"/>
+  <animation>
+   <frame tileid="28" duration="450"/>
+   <frame tileid="30" duration="450"/>
+   <frame tileid="29" duration="450"/>
+  </animation>
+ </tile>
+ <tile id="29">
+  <image width="64" height="96" source="SSD_LOGO5_2014schraeg64rtuerkis.png"/>
+ </tile>
+ <tile id="30">
+  <image width="64" height="96" source="SSD_LOGO5_2014schraeg64rblau.png"/>
+ </tile>
+ <tile id="31">
+  <image width="64" height="96" source="SSD_LOGO5_2014schraeg64lTuerkis.png"/>
+ </tile>
+ <tile id="32">
+  <image width="64" height="96" source="SSD_LOGO5_2014schraeg64lBlau.png"/>
+ </tile>
+ <tile id="33">
+  <image width="64" height="96" source="SSD_LOGO5_2014schraeg64lMagenta.png"/>
+  <animation>
+   <frame tileid="33" duration="450"/>
+   <frame tileid="32" duration="450"/>
+   <frame tileid="31" duration="450"/>
+  </animation>
+ </tile>
+</tileset>
